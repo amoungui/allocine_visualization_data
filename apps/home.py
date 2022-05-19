b5740cfb2b828c9dce686c0594347c38805263c1
Scaffolder <54202856+amoungui@users.noreply.github.com>
2022-05-19 12:25:07 +0200

    change dataset

--- apps/home.py
+++ apps/home.py
@@ -13,7 +13,7 @@ from Utils.Plotlyfy import Plotlyfy as ply
 # set the style for seaborn
 sns.set_style('whitegrid') 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-dataframe = 'https://samoungui.com/wp-content/uploads/2022/04/allocine_movies_brute.csv'
+dataframe = 'https://samoungui.com/wp-content/uploads/2022/05/allocine_movies_brute.csv'
 
 def app():
     st.title('DashBoard')
