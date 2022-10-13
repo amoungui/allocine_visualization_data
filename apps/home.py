cc91009d16bfc33d6241d6932412ba677587f8d7
Scaffolder <54202856+amoungui@users.noreply.github.com>
2022-10-13 09:05:49 +0200

    Change filename url

--- apps/home.py
+++ apps/home.py
@@ -13,7 +13,7 @@ from Utils.Plotlyfy import Plotlyfy as ply
 # set the style for seaborn
 sns.set_style('whitegrid') 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-dataframe = 'https://ecoshop.passions-deco.com/wp-content/uploads/2022/06/allocine_movies_brute.csv'
+dataframe = '../data/allocine_movies_brute.csv'
 
 def app():
     st.title('DashBoard')
