02ca8ebe26e7116f7b21c73f054308c2da1716cf
Scaffolder <54202856+amoungui@users.noreply.github.com>
2023-04-22 11:51:40 +0200

    Change url of csv file

--- apps/home.py
+++ apps/home.py
@@ -13,7 +13,7 @@ from Utils.Plotlyfy import Plotlyfy as ply
 # set the style for seaborn
 sns.set_style('whitegrid') 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-dataframe = 'data/allocine_movies_brute.csv'
+dataframe = 'https://www.amoungui.com/allocine_movies_brute.csv'
 
 def app():
     st.title('DashBoard')
